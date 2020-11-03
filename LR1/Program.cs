881cc7a64ab88af1485c2028496de36034d42add
iwbas <brexceed@gmail.com>
2020-11-03 12:55:17 +0400

    first commit

---
+++ LR1/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Threading;
+
+namespace LR1
+{
+    class Program
+    {
+        const UInt16 N = 4;
+        const String literals = "ABCDF";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Последовательная обработка");
+
+            for (ushort c = 0; c < N + 1; c++)
+            {
+                Console.WriteLine("Последовательно программ: " + (c + 1));
+                DateTime t1 = DateTime.Now;
+                for (ushort k = 0; k <= c; k++)
+                {
+                    double x = 12345.6789;
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        for (int j = 0; j < 10000; j++)
+                        {
+                            x = Math.Sqrt(x);
+                            x = x + 0.000000001;
+                            x = Math.Pow(x, 2);
+                        }
+                    }
+                    Console.WriteLine(literals[k].ToString() + ": " + x.ToString());
+                }
+                DateTime t2 = DateTime.Now;
+                Console.WriteLine(t2 - t1);
+            }
+
+            Console.WriteLine("Многопоточная обработка с лямбда выражением");
+
+            multithreading((state) =>
+            {
+                double x = 12345.6789;
+
+                for (int i = 0; i < 10000; i++)
+                {
+                    for (int j = 0; j < 10000; j++)
+                    {
+                        x = Math.Sqrt(x);
+                        x = x + 0.000000001;
+                        x = Math.Pow(x, 2);
+                    }
+                }
+
+                Console.WriteLine(Thread.CurrentThread.Name + ": " + x.ToString());
+
+                ManualResetEvent e = state as ManualResetEvent;
+                e.Set();
+            });
+
+            Console.WriteLine("Многопоточная обработка со статической процедурой");
+
+            multithreading(JobForAThread);
+
+            Console.WriteLine("Многопоточная обработка со статической процедурой и пулом потоков");
+            for (ushort i = 0; i < N + 1; i++)
+            {
+                ushort n = (ushort)(i + 1);
+                var events = new ManualResetEvent[n];
+                Console.WriteLine("Потоков: " + n);
+                DateTime t1 = DateTime.Now;
+                for (UInt16 j = 0; j <= i; j++)
+                {
+                    events[j] = new ManualResetEvent(false);
+                    ThreadPool.QueueUserWorkItem(JobForAThread, events[j]);
+                }
+                WaitHandle.WaitAll(events);
+                DateTime t2 = DateTime.Now;
+                Console.WriteLine(t2 - t1);
+            }
+
+            Console.WriteLine("Многопоточная обработка с разными приоритетами");
+            for (ushort i = 0; i < N + 1; i++)
+            {
+                ushort threadAmount = (ushort)(i + 1);
+                var events = new ManualResetEvent[threadAmount];
+
+                Console.WriteLine("Параллельных потоков: " + (threadAmount));
+
+                DateTime t1 = DateTime.Now;
+
+                for (UInt16 j = 0; j < threadAmount; j++)
+                {
+                    events[j] = new ManualResetEvent(false);
+                    Thread thread = new Thread(new ParameterizedThreadStart(JobForAThread));
+
+                    if (j % 2 == 0) 
+                        thread.Priority = ThreadPriority.Highest;
+                    else
+                        thread.Priority = ThreadPriority.Lowest;
+
+                    thread.Name = literals[j].ToString();
+                    thread.Start(events[j]);
+                }
+                WaitHandle.WaitAll(events);
+
+                DateTime t2 = DateTime.Now;
+                Console.WriteLine(t2 - t1);
+            }
+        }
+
+        public static void multithreading(Action<object> func)
+        {
+            for (ushort i = 0; i < N + 1; i++)
+            {
+                ushort threadAmount = (ushort)(i + 1);
+                var events = new ManualResetEvent[threadAmount];
+
+                Console.WriteLine("Параллельных потоков: " + (threadAmount));
+
+                DateTime t1 = DateTime.Now;
+
+                for (UInt16 j = 0; j < threadAmount; j++)
+                {
+                    events[j] = new ManualResetEvent(false);
+
+                    Thread thread = new Thread(new ParameterizedThreadStart(func));
+
+                    thread.Name = literals[j].ToString();
+                    thread.Start(events[j]);
+                }
+                WaitHandle.WaitAll(events);
+
+                DateTime t2 = DateTime.Now;
+                Console.WriteLine(t2 - t1);
+            }
+        }
+
+        static void JobForAThread(object state)
+        {
+            double x = 12345.6789;
+            for (int i = 0; i < 10000; i++)
+            {
+                for (int j = 0; j < 10000; j++)
+                {
+                    x = Math.Sqrt(x);
+                    x = x + 0.000000001;
+                    x = Math.Pow(x, 2);
+                }
+            }
+            Console.WriteLine(Thread.CurrentThread.Name + " " + Thread.CurrentThread.ManagedThreadId + ": " + x.ToString());
+
+            ManualResetEvent ev = state as ManualResetEvent;
+            ev.Set();
+        }
+    }
+}
---
+++ LR2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace LR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа 2. Выполнил студент ДИПРБ-41 Дабысов А.С.");
+            var N = new List<int>() { 10, 100, 1000, 100000, 10000000 }; // число элементов вектора
+            var M = new List<int>() { 2, 3, 4, 5, 10 };        // число потоков
+
+            Console.WriteLine("=================================================================================================================");
+            Console.WriteLine("Анализ эффективности многопоточной обработки при разных параметрах N (10, 100, 1000, 100000) и M (2, 3, 4, 5, 10)");
+
+            for (int i = 0; i < N.Count; i++) {
+                int n = N[i];
+                Console.WriteLine("============ " + "N: " + n + " ============");
+
+                for (int j = 0; j < M.Count; j++)  {
+                    int m = M[j];
+                    Console.WriteLine("============ " + "M: " + m + " ============");
+
+                    var list = new List<int>(Enumerable.Range(0, n));
+
+                    // 1. Последовательная обработка элементов вектора
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+
+                    for (int k = 0; k < n; k++)
+                        list[k] = (int)Math.Pow(list[k], 2);
+
+                    sw.Stop();
+                    TimeSpan ts = sw.Elapsed;
+                    Console.WriteLine("1. Total time: {0}", ts.TotalMilliseconds);
+
+                    // 2. Многопоточная обработка элементов вектора, 
+                    //    используя разделение вектора на равное число элементов. 
+
+                    sw.Start();
+
+                    int blockSize = n / m;
+                    var events = new ManualResetEvent[m];
+
+                    for (int k = 0; k < m; k++)
+                    {
+                        int start = k * blockSize;
+                        int end = (k * blockSize + blockSize);
+                        if (end > n)
+                            end = n;
+
+                        var e = events[k] = new ManualResetEvent(false);
+                        
+                        Thread thread = new Thread(() => PowerList(list, start, end, e));
+                        thread.Name = k.ToString();
+
+                        thread.Start();
+                    }
+
+                    WaitHandle.WaitAll(events);
+
+                    sw.Stop();
+                    ts = sw.Elapsed;
+                    Console.WriteLine("2. Total time: {0}", ts.TotalMilliseconds);
+
+                    // 4. Анализ эффективности при усложнении обработки каждого элемента вектора.
+
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static void PowerList(List<int> list, int start, int end, ManualResetEvent state)
+        {
+            for (int i = start; i < end; i++)
+                list[i] = (int)Math.Pow(list[i], 2);
+
+            state.Set();
+        }
+    }
+}
