c4201c12410009987346c5a2f8b0db7007bedbc5
iwbas <brexceed@gmail.com>
2020-11-04 12:51:37 +0400

    LR3 initial

---
+++ LR3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LR3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №3. Простые числа.");
+            Console.WriteLine("Выполнил студент ДИПРБ-41 Дабысов А.С.");
+
+            int n = 20;
+            List<int> numbers = new List<int>(Enumerable.Range(2, n));
+            List<int> primeNumbers = new List<int>(capacity: numbers.Count);
+
+            // Последовательное решето Эратосфена
+            for (int m = 2; m < Math.Sqrt(n); m++)
+            {
+                
+            }
+
+        }
+    }
+}
